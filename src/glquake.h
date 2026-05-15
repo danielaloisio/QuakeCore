a466232a3b9ac54d7194f5040d0f9e0860264d48
daniel.aloisio1 <daniel.aloisio1@gmail.com>
2026-05-15 18:26:11 -0300

    Alter glquake

--- src/glquake.h
+++ src/glquake.h
@@ -29,10 +29,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 
 #ifdef __APPLE__
-#include <OpenGL/gl.h>
 #include <OpenGL/glu.h>
 #else
-#include <GL/gl.h>
 #include <GL/glu.h>
 #endif
 
